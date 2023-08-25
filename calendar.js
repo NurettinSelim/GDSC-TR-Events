99bc4fd9116619a365875387f379da18b8c65a54
NurettinSelim <nurettinselim03@gmail.com>
2023-08-25 17:51:10 +0300

    updated to gen 2 functions

--- calendar.js
+++ calendar.js
@@ -1,5 +1,8 @@
 const { google } = require('googleapis');
 const logger = require("firebase-functions/logger");
+const { defineString } = require('firebase-functions/params');
+
+const CALENDAR_ID = defineString('CALENDAR_ID').value();
 
 const auth = new google.auth.GoogleAuth({
     scopes: ['https://www.googleapis.com/auth/calendar'],
@@ -21,18 +24,25 @@ module.exports.createEvent = function (eventData) {
             'timeZone': 'America/Los_Angeles',
         },
     };
-    gcal.events.insert({
-        calendarId: process.env.CALENDAR_ID,
+    logger.info({
+        calendarId: CALENDAR_ID,
         resource: eventCal,
-    }, function (err, event) {
-        if (err) {
-            logger.error('There was an error contacting the Calendar service: ', err)
-            logger.info('Event data was:', eventCal)
-            return;
-        }
-        logger.info('Event created: ', event.data);
-    });
-
+    })
+    return new Promise((resolve, reject) => {
+        gcal.events.insert({
+            calendarId: CALENDAR_ID,
+            resource: eventCal,
+        }, function (err, event) {
+            if (err) {
+                logger.error('There was an error contacting the Calendar service: ', err)
+                logger.info('Event data was:', eventCal)
+                reject(err)
+                return;
+            }
+            logger.info('Event created: ', event.data);
+            resolve(event.data)
+        });
+    })
 }
 
 
@@ -50,7 +60,7 @@ module.exports.updateEvent = function (eventData) {
         },
     };
     gcal.events.update({
-        calendarId: process.env.CALENDAR_ID,
+        calendarId: CALENDAR_ID,
         eventId: `${eventData.id}`,
         resource: eventCal,
     }, function (err, event) {
@@ -67,7 +77,7 @@ module.exports.updateEvent = function (eventData) {
 
 module.exports.deleteEvent = function (eventData) {
     gcal.events.delete({
-        calendarId: process.env.CALENDAR_ID,
+        calendarId: CALENDAR_ID,
         eventId: `${eventData.id}`,
     }, function (err, event) {
         if (err) {
--- index.js
+++ index.js
@@ -1,74 +1,90 @@
-const functions = require("firebase-functions");
-const admin = require('firebase-admin');
-const axios = require('axios');
+const { setGlobalOptions, logger } = require("firebase-functions/v2");
+const { initializeApp } = require('firebase-admin/app');
+const { getFirestore } = require('firebase-admin/firestore');
+const { get } = require('axios');
+const { onSchedule } = require("firebase-functions/v2/scheduler");
+const { onDocumentCreated, onDocumentUpdated, onDocumentDeleted } = require("firebase-functions/v2/firestore");
 
-const calendar = require("./calendar")
+const { createEvent, updateEvent, deleteEvent } = require("./calendar");
 
-admin.initializeApp();
+initializeApp();
+setGlobalOptions({ maxInstances: 5 })
 
-const db = admin.firestore();
+const db = getFirestore();
 const eventsCollection = db.collection("events");
 
-exports.eventChecker = functions
-    .runWith({ timeoutSeconds: 300 })
-    .pubsub.schedule('0 0 * * *')
-    .onRun(async (context) => {
-        
-        const startDate = new Date(2023, 2, 1).toISOString().substring(0, 10);
-        const endDate = new Date(Date.now() + 52 * 7 * 24 * 60 * 60 * 1000).toISOString().substring(0, 10);
+const startDate = new Date(2023, 5, 1).toISOString().substring(0, 10);
+const endDate = new Date(Date.now() + 52 * 7 * 24 * 60 * 60 * 1000).toISOString().substring(0, 10);
 
-        var data = { "links": { "next": `https://gdsc.community.dev/api/event/?fields=id,chapter,title,status,start_date,end_date,url&status=Published&start_date=${startDate}&end_date=${endDate}` } };
+const fetchEvents = async (context) => {
+    var data = { "links": { "next": `https://gdsc.community.dev/api/event/?fields=id,chapter,title,status,start_date,end_date,url&status=Published&start_date=${startDate}&end_date=${endDate}` } };
 
-        const savedIds = (await eventsCollection.get()).docs.map(function (doc) {
-            return doc.id;
+    const savedIds = (await eventsCollection.get()).docs.map(function (doc) {
+        return doc.id;
+    });
+
+    while (data["links"]["next"] != null) {
+        const batch = db.batch();
+
+        var resp = await get(data["links"]["next"]);
+        var data = resp.data;
+        logger.debug(data["links"]["next"]);
+
+
+        data["results"].forEach(function (event) {
+            if (event["chapter"]["country"] == "TR") {
+                batch.set(eventsCollection.doc(event["id"].toString()), event)
+                if (savedIds.includes(event["id"].toString())) {
+                    savedIds.splice(savedIds.indexOf(event["id"].toString()), 1);
+                }
+            }
         });
 
+        await batch.commit();
+    }
+    for (let i = 0; i < savedIds.length; i++) {
+        await eventsCollection.doc(savedIds[i]).delete();
+    }
+    return null;
+}
 
-        while (data["links"]["next"] != null) {
-            const batch = db.batch();
-
-            var resp = await axios.get(data["links"]["next"]);
-            var data = resp.data;
-            functions.logger.debug(data["links"]["next"]);
-
-
-            data["results"].forEach(function (event) {
-                if (event["chapter"]["country"] == "TR") {
-                    batch.set(eventsCollection.doc(event["id"].toString()), event)
-                    if (savedIds.includes(event["id"].toString())) {
-                        savedIds.splice(savedIds.indexOf(event["id"].toString()), 1);
-                    }
-                }
-            });
-
-            await batch.commit();
-        }
-        for (let i = 0; i < savedIds.length; i++) {
-            await eventsCollection.doc(savedIds[i]).delete();
-        }
-        return null;
+// fetch events that could not be added to the calendar due to error
+const fetchEventsWithoutCalendar = async (event) => {
+    const snapshot = await db.collection("events").where("isAddedToCalendar", "==", false).get()
+    snapshot.docs.map(async function (doc) {
+        const eventData = doc.data();
+        await createEvent(eventData);
+        await doc.ref.update({ isAddedToCalendar: true });
     });
+    return null;
+}
 
-exports.databaseOnCreate = functions.firestore
-    .document("events/{eventId}")
-    .onCreate((snap, context) => {
-        const eventData = snap.data();
-        calendar.createEvent(eventData);
-        return null;
-    });
+exports.eventChecker = onSchedule({ schedule: '0 0 * * *', timeoutSeconds: 300 }, fetchEvents)
 
-exports.databaseOnUpdate = functions.firestore
-    .document("events/{eventId}")
-    .onUpdate((change, context) => {
-        const newEventData = change.after.data();
-        calendar.updateEvent(newEventData);
-        return null;
-    });
+exports.eventWithoutCalenderChecker = onSchedule({ schedule: '0 12 * * *', timeoutSeconds: 300 }, fetchEventsWithoutCalendar)
 
-exports.databaseOnDelete = functions.firestore
-    .document("events/{eventId}")
-    .onDelete((snap, context) => {
-        const deletedData = snap.data();
-        calendar.deleteEvent(deletedData);
-        return null;
-    });
+exports.databaseOnCreate = onDocumentCreated("events/{eventId}", async (event) => {
+    const eventData = event.data.data();
+    try {
+        await createEvent(eventData);
+        await event.data.ref.update({ isAddedToCalendar: true });
+    } catch (e) {
+        logger.error(e);
+        await event.data.ref.update({ isAddedToCalendar: false });
+    }
+    return null;
+})
+
+exports.databaseOnUpdate = onDocumentUpdated("events/{eventId}", (event) => {
+    if (event.data.before.data().isAddedToCalendar == false && event.data.after.data().isAddedToCalendar == true) {
+        const newEventData = event.data.after.data();
+        updateEvent(newEventData);
+    }
+    return null;
+})
+
+exports.databaseOnDelete = onDocumentDeleted("events/{eventId}", (event) => {
+    const deletedData = event.data.data();
+    deleteEvent(deletedData);
+    return null;
+})
